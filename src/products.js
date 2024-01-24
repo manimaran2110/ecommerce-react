be50119b002c551d0c3e953add2c5bb126038c95
MANIMARAN V <100070578+manimaran2110@users.noreply.github.com>
2024-01-24 13:34:17 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from './assests/product1.png';
+import product2 from './assests/product2.png';
+import product3 from './assests/product3.png';
+import product4 from './assests/product4.png';
+import product5 from './assests/product5.png';
+import product6 from './assests/product6.png';
+import product7 from './assests/product7.png';
+import product8 from './assests/product8.png';
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Macbook",
+        price: 1500.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Canon Camera",
+        price: 950.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Denim Jacket",
+        price:  228.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "LED Light Strips",
+        price: 20.12,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Spectrum LS TEE",
+        price: 68.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Shirt",
+        price: 50.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Hat",
+        price: 25.0,
+        productImage: product8,
+    },
+];
